e51cab6e567a462cb41f0926108aa3734b2e1c6e
Hướng Ngọc Trâm <tramhuong.31231022514@st.ueh.edu.vn>
2024-10-02 08:48:59 +0700

    Add files via upload

---
+++ Exercises_4.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamental_of_Programming
+{
+    internal class Exercises_4
+    {
+        static void _Main()
+        {
+            int[] arr = GenerateRandomArray(10, 1, 100);
+
+            Console.WriteLine("Initial array: ");
+            PrintArray(arr);
+
+            double average = CalculateAverage(arr);
+            Console.WriteLine("Average value: " + average);
+
+            int searchValue = 50;
+            bool containsValue = ContainsValue(arr, searchValue);
+            Console.WriteLine("Array contains " + searchValue + ": " + containsValue);
+
+            int index = FindIndex(arr, searchValue);
+            Console.WriteLine("Index of " + searchValue + ": " + (index == -1 ? "Not found" : index.ToString()));
+
+            arr = RemoveElement(arr, searchValue);
+            Console.WriteLine("Array after removing " + searchValue + ": ");
+            PrintArray(arr);
+
+            int maxValue = FindMaxValue(arr);
+            int minValue = FindMinValue(arr);
+            Console.WriteLine("Max value: " + maxValue);
+            Console.WriteLine("Min value: " + minValue);
+
+            arr = ReverseArray(arr);
+            Console.WriteLine("Array after reversing: ");
+            PrintArray(arr);
+
+            int[] duplicates = FindDuplicates(arr);
+            Console.WriteLine("Duplicate values: ");
+            PrintArray(duplicates);
+
+            arr = RemoveDuplicates(arr);
+            Console.WriteLine("Array after removing duplicates: ");
+            PrintArray(arr);
+        }
+
+        static int[] GenerateRandomArray(int length, int minValue, int maxValue)
+        {
+            Random random = new Random();
+            int[] arr = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = random.Next(minValue, maxValue);
+            }
+            return arr;
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static double CalculateAverage(int[] arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum / arr.Length;
+        }
+
+        static bool ContainsValue(int[] arr, int value)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == value)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static int FindIndex(int[] arr, int value)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == value)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static int[] RemoveElement(int[] arr, int value)
+        {
+            int newLength = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != value)
+                {
+                    newLength++;
+                }
+            }
+
+            int[] newArr = new int[newLength];
+            int j = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != value)
+                {
+                    newArr[j] = arr[i];
+                    j++;
+                }
+            }
+            return newArr;
+        }
+
+        static int FindMaxValue(int[] arr)
+        {
+            int maxValue = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > maxValue)
+                {
+                    maxValue = arr[i];
+                }
+            }
+            return maxValue;
+        }
+
+        static int FindMinValue(int[] arr)
+        {
+            int minValue = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < minValue)
+                {
+                    minValue = arr[i];
+                }
+            }
+            return minValue;
+        }
+
+        static int[] ReverseArray(int[] arr)
+        {
+            int[] reversedArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                reversedArr[i] = arr[arr.Length - 1 - i];
+            }
+            return reversedArr;
+        }
+
+        static int[] FindDuplicates(int[] arr)
+        {
+            int[] duplicates = new int[arr.Length];
+            int duplicatesCount = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        duplicates[duplicatesCount] = arr[i];
+                        duplicatesCount++;
+                        break;
+                    }
+                }
+            }
+
+            int[] result = new int[duplicatesCount];
+            for (int i = 0; i < duplicatesCount; i++)
+            {
+                result[i] = duplicates[i];
+            }
+            return result;
+        }
+
+        static int[] RemoveDuplicates(int[] arr)
+        {
+            int[] newArr = new int[arr.Length];
+            int newArrCount = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = 0; j < newArrCount; j++)
+                {
+                    if (arr[i] == newArr[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate)
+                {
+                    newArr[newArrCount] = arr[i];
+                    newArrCount++;
+                }
+            }
+
+            int[] result = new int[newArrCount];
+            for (int i = 0; i < newArrCount; i++)
+            {
+                result[i] = newArr[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ Exercises_5.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamental_of_Programming
+{
+    internal class Exercises_5
+    {
+        static void _Main(string[] args)
+        {
+            //viết chương trình cho user chọn dạng menu
+            //Console.Write("Enter the No rows: ");
+            //int rows = int.Parse(Console.ReadLine());
+            int rows = 0;
+            int[][] a = new int[rows][];
+            init_random(a,rows);
+            print(a);
+            Console.WriteLine();
+            while (true)
+            {
+                int cho = menu();
+                switch (cho)
+                {
+                    case 0: Console.WriteLine("Bye bye!");return;
+                    case 1: biggest_number(a,rows); break;
+                    case 2: sort_rows(a); break;
+                    case 3: print_prime(a); break;
+                    case 4: print_positions(a); break;
+                }
+            }
+        }
+        static int menu()
+        {
+            Console.WriteLine("1. Print the biggest number of each row and the largest number of the whole array.");
+            Console.WriteLine("2. Sort values ascending of each row");
+            Console.WriteLine("3. Print items of the array that are prime.");
+            Console.WriteLine("4. Search and print all positions of a number (enter from the user).");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine();
+            Console.WriteLine("You need to enter <1..4> to select the task to performed");
+            Console.WriteLine("If you have done all task, please enter '0' to Exit");
+            int cho = 0;
+            while (true)
+            {
+                bool c = int.TryParse(Console.ReadLine(), out cho);
+                if (c && cho >= 0 && cho <= 6)
+                    break;
+                else Console.WriteLine("Please enter a valid choice!");
+            }
+            return cho;
+        }
+        static void init_random(int[][] a, int rows)
+        {     
+            Random rnd = new Random();
+            int row = rnd.Next(3, 10);
+            for (int i = 0; i < row; i++)
+            {
+                //Console.Write($"Enter the No columns of the row {i} th: ");
+                //int cols = int.Parse(Console.ReadLine()); 
+
+                a[i] = new int[row];
+                for (int j = 0; j < row; j++)
+                    a[i][j] = rnd.Next(10, 50);
+            }
+        }
+        static void init_from_user(int[][] a)
+        {
+
+        }
+        static void print(int[][] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a[i].Length; j++)
+                    Console.Write(a[i][j] + " ");
+                Console.WriteLine();
+            }
+        }
+        static void biggest_number(int[][] a, int rows )
+        {
+
+        }
+        static void sort_array(int[] a)
+        {
+            for(int i = 0;i < a.Length;i++)
+                for(int j = 0; j<a.Length;j++)
+                    if (a[i] < a[j])
+                    {
+                        int temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+        }
+        static void sort_rows(int[][] a)
+        {
+            for(int i = 0;i < a.Length; i++)
+                sort_array(a[i]);
+        }
+        static bool isPrime(int num)
+        {
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static void print_prime(int[][] a)
+        {
+            for (int i = 0; i <= a.Length; i++)
+            {
+                for (int j = 0; j < a[i].Length; j++)
+                    if (isPrime(a[i][j]))
+                    Console.WriteLine($"{a[i][j]} appears at row [{i}], col [{j}]");
+            }
+        }
+        static void positions(int[][] a)
+        {
+
+        }     
+        static void print_positions(int[][] a)
+        {
+
+        }
+    }
+}
+
